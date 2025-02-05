225dcb813f4cebb9dad0154053e4bb5bbc7a9f4c
team25674 <team25674@broomfieldstem.org>
2025-02-04 20:12:52 -0700

    Changed claw values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/production/AutoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/production/AutoPark.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.teamcode.lib.mechanisms.LinearSlide;
 
 
 
-@Autonomous(name = "Autopark", group = "Auto")
+@Autonomous(name = "AutoBasket", group = "Auto")
 public class AutoPark extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
