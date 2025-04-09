59b87c2037b283eb50b19bf44b8a60c938c08d91
team25674 <team25674@broomfieldstem.org>
2025-04-08 20:28:29 -0600

    Adjustments on constant values for claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/mechanisms/Claw.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.lib.mechanisms;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
-    private final double GRAB_POSITION = 1;
-    private final double BASKET_POSTION = 0.;
+    private final double GRAB_POSITION = .6;
+    private final double BASKET_POSTION = -.1;
     private final double OPEN_POSITION_CLAWSERVO = .6;
     private final double CLOSE_POSITION_CLAWSERVO = .9;
 
